de14ee3a8da4f63591e36404dccee4beeeedb73a
Ahmad Hashemi <36945477+towardsNLP@users.noreply.github.com>
2026-01-27 14:13:50 -0500

    Update Playground.py

--- Playground.py
+++ Playground.py
@@ -28,6 +28,7 @@ import phrasetopicminer as ptm
 from dotenv import load_dotenv
 load_dotenv(override=True)
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+PASSWORD = os.getenv("PASSWORD")
 
 # Import local modules
 from src import (
